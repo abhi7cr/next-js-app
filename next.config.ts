34361af133c27ec0183e016279f2fb1486abba54
Abhishek Raj <abhi7cronaldo@gmail.com>
2024-11-06 09:12:19 -0800

    use absolute path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: 'https://main.dd23bxkccej1t.amplifyapp.com/assets'
+  assetPrefix: '/assets'
 };
 
 export default nextConfig;
